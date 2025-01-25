ff95562174ece1480a6ab82b9d10c85f709b0fb7
Nishtha <132751527+nishtha-902@users.noreply.github.com>
2025-01-25 14:51:16 +0530

    Update ExpenseList.js

--- client/src/components/ExpenseList.js
+++ client/src/components/ExpenseList.js
@@ -7,7 +7,7 @@ const ExpenseList = () => {
     useEffect(() => {
         const fetchExpenses = async () => {
             const token = localStorage.getItem('token');
-            const { data } = await axios.get('http://localhost:8000/api/expenses', {
+            const { data } = await axios.get('https://expense-project-bg3c.onrender.com/api/expenses', {
                 headers: { Authorization: `Bearer ${token}` },
             });
             setExpenses(data);
