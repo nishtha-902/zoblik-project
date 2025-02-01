1fa8c837feb36fed0cfaff22e23cd9ff2c7acdcd
Nishtha <132751527+nishtha-902@users.noreply.github.com>
2025-02-01 11:54:42 +0530

    Update ExpenseList.js

--- client/src/components/ExpenseList.js
+++ client/src/components/ExpenseList.js
@@ -1,21 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ExpenseList = ({ expenses, setExpenses }) => {
+const ExpenseList = () => {
+    const [expenses, setExpenses] = useState([]);
+
     useEffect(() => {
         const fetchExpenses = async () => {
-            try {
-                const token = localStorage.getItem('token');
-                const { data } = await axios.get('https://zoblik-project-backend.onrender.com/api/expenses', {
-                    headers: { Authorization: `Bearer ${token}` },
-                });
-                setExpenses(data);
-            } catch (error) {
-                console.error("Error fetching expenses:", error);
-            }
+            const token = localStorage.getItem('token');
+            const { data } = await axios.get('https://zoblik-project-backend.onrender.com/api/expenses', {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            setExpenses(data);
         };
         fetchExpenses();
-    }, [setExpenses]); // Updates when expenses change
+    }, []);
 
     return (
         <ul>
@@ -29,36 +27,3 @@ const ExpenseList = ({ expenses, setExpenses }) => {
 };
 
 export default ExpenseList;
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// const ExpenseList = () => {
-//     const [expenses, setExpenses] = useState([]);
-
-//     useEffect(() => {
-//         const fetchExpenses = async () => {
-//             const token = localStorage.getItem('token');
-//             const { data } = await axios.get('https://zoblik-project-backend.onrender.com/api/expenses', {
-//                 headers: { Authorization: `Bearer ${token}` },
-//             });
-//             setExpenses(data);
-//         };
-//         fetchExpenses();
-//     }, []);
-
-//     return (
-//         <ul>
-//             {expenses.map((expense) => (
-//                 <li key={expense._id}>
-//                     {expense.amount} - {expense.category} - {expense.description}
-//                 </li>
-//             ))}
-//         </ul>
-//     );
-// };
-
-// export default ExpenseList;
